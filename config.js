194138d51fec538d55a9ee69af3987d0d3d05114
ArslanMD Official <numberkliye@gmail.com>
2025-08-05 19:58:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,8 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/qpnazl.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "",
+MENU_VIDEO_URL: process.env.MENU_VIDEO_URL || "https://files.catbox.moe/wzdxwt.mp4",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
